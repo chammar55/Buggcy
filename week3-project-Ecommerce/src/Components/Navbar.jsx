3b581a11c68dd69fbc110d4422b00db5754e5ae4
Muhammad Ammar Ibrahim <96951639+chammar55@users.noreply.github.com>
2024-08-02 19:53:20 +0500

    .

--- week3-project-Ecommerce/src/Components/Navbar.jsx
+++ week3-project-Ecommerce/src/Components/Navbar.jsx
@@ -8,17 +8,17 @@ function Navbar() {
   return (
     <div className="flex justify-between align p-2 px-6 sm:px-5 max-w-[1440px] mx-auto ">
       <h1>
-        <Link className="text-lg sm:text-3xl font-bold" to="/">
+        <Link className="text-lg sm:text-2xl font-bold" to="/">
           Ecomm
         </Link>
       </h1>
       <h1>
-        <Link className="text-3xl sm:text-5xl relative" to="/CartPage">
+        <Link className="text-3xl sm:text-4xl relative" to="/CartPage">
           <ion-icon className="relative" name="cart-outline"></ion-icon>
           {userdata.length === 0 ? (
-            " "
+            ""
           ) : (
-            <p className="text-[12px] bg-red-500 rounded-full w-[20px] h-[20px] flex justify-center items-center text-white absolute top-4 -right-1 sm:top-7 sm:-right-0 xxl:top-7 xxl:right-0">
+            <p className="text-[11px] bg-red-500 rounded-full w-[18px] h-[18px] flex justify-center items-center text-white absolute top-4 -right-1 sm:top-5 sm:-right-0 xxl:top-5 xxl:right-0">
               {userdata.length}
             </p>
           )}
