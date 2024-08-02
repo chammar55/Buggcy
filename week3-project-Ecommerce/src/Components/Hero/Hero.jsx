c4de5a25aebc627d04fa8cf59792bf0772af2b0f
Muhammad Ammar Ibrahim <96951639+chammar55@users.noreply.github.com>
2024-08-02 20:07:26 +0500

    .

--- week3-project-Ecommerce/src/Components/Hero/Hero.jsx
+++ week3-project-Ecommerce/src/Components/Hero/Hero.jsx
@@ -7,6 +7,14 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Pagination, Navigation } from "swiper/modules";
 
 function Hero() {
+  const handleShopNowClick = () => {
+    // Find the section to scroll to
+    const section = document.getElementById("men's-clothing");
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
     <div>
       <Swiper
@@ -23,19 +31,18 @@ function Hero() {
         pagination={{
           clickable: true,
         }}
-        // navigation={true}
         modules={[Autoplay, Pagination, Navigation]}
-        // onAutoplayTimeLeft={onAutoplayTimeLeft}
         className="mySwiper"
       >
         {SwiperList.map((data, index) => {
           return (
             <SwiperSlide
-              className="w-full h-[60vw] md:h-[600px] flex justify-end "
+              key={index}
+              className="w-full h-[60vw] md:h-[600px] flex justify-end"
               style={{ backgroundColor: data.bgcolor }}
             >
-              <div className="grid grid-cols-2 max-w-[1440px] mx-auto ">
-                <div className="flex flex-col gap-7 items-start justify-center p-5 ">
+              <div className="grid grid-cols-2 max-w-[1440px] mx-auto">
+                <div className="flex flex-col gap-7 items-start justify-center p-5">
                   <p className="text-[2vw] md:text-2xl lg:text-3xl">
                     {data.sale}
                   </p>
@@ -43,13 +50,14 @@ function Hero() {
                     {data.title}
                   </h1>
                   <button
-                    className="text-[3vw] p-[1vw]  rounded-md md:text-3xl md:px-4 md:py-3"
+                    onClick={handleShopNowClick}
+                    className="text-[3vw] p-[1vw] rounded-md md:text-3xl md:px-4 md:py-3"
                     style={{ backgroundColor: data.button }}
                   >
                     Shop Now
                   </button>
                 </div>
-                <div className="relative  xxl:flex xxl:justify-end ">
+                <div className="relative xxl:flex xxl:justify-end">
                   <img
                     className="lg:max-w-[550px] absolute bottom-0"
                     src={data.image}
