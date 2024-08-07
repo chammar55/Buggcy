bb9ad35ec2fbd00473a62125ae560602e5ce3749
Muhammad Ammar Ibrahim <96951639+chammar55@users.noreply.github.com>
2024-08-07 20:41:42 +0500

    .

---
+++ week4-project-authentication-system/src/components/DarkModeButton.tsx
@@ -0,0 +1,17 @@
+"use client";
+import React, { useState } from "react";
+import { Moon, Sun } from "lucide-react";
+
+const DarkModeButton = () => {
+  const [darkMode, setDarkMode] = useState(false);
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+  return (
+    <div>
+      <button onClick={toggleDarkMode}>{darkMode ? <Moon /> : <Sun />}</button>
+    </div>
+  );
+};
+
+export default DarkModeButton;
